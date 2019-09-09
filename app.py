3542927a56c85b998536368a9a885c6b85f57d5f
Volltin <volltin@live.com>
2019-09-09 15:00:24 +0800

    Add upstream api

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask import Response, abort
+from flask import Response, abort, request
 import requests
 
 from config import DefaultConfig as Conf
@@ -7,6 +7,23 @@ from filters import HideDetail
 
 app = Flask(__name__)
 
+@app.route('/<key>/upstream.ics')
+def upstream_ics(key):
+    if hasattr(Conf, "direct_token") and Conf.direct_token:
+        token = str(request.args.get('token'))
+        if not token == Conf.direct_token:
+            abort(403)
+    
+    upstream_url = Conf.upstreams.get(key)
+    if upstream_url:
+        r = requests.get(upstream_url)
+        output = r.content
+        if r.status_code == 200:
+            return Response(output, mimetype='text/calendar')
+        else:
+            abort(r.status_code)
+    else:
+        abort(404)
 
 @app.route('/<key>/filtered.ics')
 def filtered_ics(key):
